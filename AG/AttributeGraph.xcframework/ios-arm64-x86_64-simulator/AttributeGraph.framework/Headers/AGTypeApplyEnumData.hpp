633cf6924f5cdacf35b6f4e93b599596a3141e2f
Kyle <kyle201817146@gmail.com>
2024-01-29 01:10:27 +0800

    Add OGTypeApplyEnumData

---
+++ AG/AttributeGraph.xcframework/ios-arm64-x86_64-simulator/AttributeGraph.framework/Headers/AGTypeApplyEnumData.hpp
@@ -0,0 +1,25 @@
+//
+//  AGTypeApplyEnumData.hpp
+//  
+//
+//  Created by Kyle on 2024/1/29.
+//
+
+#ifndef AGTypeApplyEnumData_hpp
+#define AGTypeApplyEnumData_hpp
+
+#include "AGBase.hpp"
+
+AG_EXTERN_C_BEGIN
+
+// TODO
+AG_EXPORT
+AG_REFINED_FOR_SWIFT
+bool AGTypeApplyEnumData();
+
+AG_EXPORT
+AG_REFINED_FOR_SWIFT
+bool AGTypeApplyMutableEnumData();
+
+AG_EXTERN_C_END
+#endif /* AGTypeApplyEnumData_hpp */
