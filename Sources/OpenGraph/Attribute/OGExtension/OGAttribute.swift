c2d292e99532fc15ad7403cd8c2e3e1f9cc9703c
Kyle <kyle201817146@gmail.com>
2024-02-17 20:01:11 +0800

    Update OGAttribute API

--- Sources/OpenGraph/Attribute/OGExtension/OGAttribute.swift
+++ Sources/OpenGraph/Attribute/OGExtension/OGAttribute.swift
@@ -9,6 +9,11 @@ import _OpenGraph
 
 extension OGAttribute {
     @inlinable
+    public init<Value>(_ attribute: Attribute<Value>) {
+        self = attribute.identifier
+    }
+    
+    @inlinable
     public func unsafeCast<Value>(to type: Value.Type) -> Attribute<Value> {
         fatalError("TODO")
     }
@@ -37,6 +42,36 @@ extension OGAttribute {
     @inlinable
     public func addInput<Value>(_ attribute: Attribute<Value>, options: OGInputOptions, token: Int) {
         addInput(attribute.identifier, options: options, token: token)
+    }
+    
+    @inlinable
+    public func visitBody<Visitor: AttributeBodyVisitor>(_ visitor: inout Visitor) {
+        fatalError("TODO")
+    }
+    
+    @inlinable
+    public func mutateBody<Value>(as: Value.Type, invalidating: Bool, _ body: (inout Value) -> ()) {
+        fatalError("TODO")
+    }
+    
+    @inlinable
+    public func breadthFirstSearch(options: OGSearchOptions, _: (OGAttribute) -> Bool) -> Bool {
+        fatalError("TODO")
+    }
+    
+    @inlinable
+    public var _bodyType: Any.Type { fatalError("TODO") }
+    
+    @inlinable
+    public var _bodyPointer: UnsafeRawPointer { fatalError("TODO") }
+    
+    @inlinable
+    public var valueType: Any.Type { fatalError("TODO") }
+        
+    @inlinable
+    public var indirectDependency: OGAttribute {
+        get { fatalError("TODO") }
+        set { fatalError("TODO") }
     }
 }
 
