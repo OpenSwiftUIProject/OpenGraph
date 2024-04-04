a116b2af0e32e1358796d25cade96739b035c2e1
Kyle <kyle201817146@gmail.com>
2024-04-04 23:45:53 +0800

    Add treeElementAPI check test case

--- Tests/OpenGraphCompatibilityTests/Graph/SubgraphTests.swift
+++ Tests/OpenGraphCompatibilityTests/Graph/SubgraphTests.swift
@@ -14,4 +14,16 @@ struct SubgraphTests {
         OGSubgraph.setShouldRecordTree()
         #expect(OGSubgraph.shouldRecordTree == true)
     }
+    
+    @Test
+    func treeElementAPICheck() {
+        let graph = OGGraph()
+        let subgraph = OGSubgraph(graph: graph)
+        subgraph.apply {
+            let value = Attribute(value: ())
+            OGSubgraph.beginTreeElement(value: value, flags: 0)
+            OGSubgraph.addTreeValue(value, forKey: "", flags: 0)
+            OGSubgraph.endTreeElement(value: value)
+        }
+    }
 }
