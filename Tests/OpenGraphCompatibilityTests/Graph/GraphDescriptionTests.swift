c35a8b766eb004139c08521dc6eaeebedc7bede6
Kyle <kyle201817146@gmail.com>
2024-04-22 01:10:52 +0800

    Fix Linux build issue

--- Tests/OpenGraphCompatibilityTests/Graph/GraphDescriptionTests.swift
+++ Tests/OpenGraphCompatibilityTests/Graph/GraphDescriptionTests.swift
@@ -4,9 +4,11 @@
 
 import Testing
 
+#if canImport(Darwin)
 struct GraphDescriptionTests {
     @Test
     func format() {
         #expect(OGGraph.descriptionFormat.takeUnretainedValue() as String == "format")
     }
 }
+#endif
