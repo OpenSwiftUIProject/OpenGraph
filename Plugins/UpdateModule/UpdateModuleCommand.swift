5d43a4ef2ef21b646353d1ed229524a224edaa9f
Kyle <kyle201817146@gmail.com>
2024-09-22 16:44:28 +0800

    Fix WASM CI issue

--- Plugins/UpdateModule/UpdateModuleCommand.swift
+++ Plugins/UpdateModule/UpdateModuleCommand.swift
@@ -10,10 +10,12 @@ import Foundation
 @main
 struct UpdateModuleCommand: CommandPlugin {
     func performCommand(context: PackagePlugin.PluginContext, arguments: [String]) async throws {
+        #if compiler(>=6.0) // Skip build on WASI 5.10
         let process = Process()
         process.executableURL = try context.tool(named: "zsh").url
         process.arguments = ["AG/update.sh"]
         try process.run()
         process.waitUntilExit()
+        #endif
     }
 }
