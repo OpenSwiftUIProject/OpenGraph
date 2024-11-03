0683beb6fa21f9875b76e89f958e8c0e656636ab
Kyle <kyle201817146@gmail.com>
2024-11-04 02:25:03 +0800

    Move tbd into a seperate folder

--- AG/update.sh
+++ AG/update.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 ## xcframework does not support soft link header file, so we use this script to simpfy the process.
 
@@ -8,9 +8,7 @@ filepath() {
 }
 
 AG_ROOT="$(dirname $(filepath $0))"
-xcframework_path="${AG_ROOT}/AttributeGraph.xcframework"
-framework_name=AttributeGraph.framework
-swiftmodule_path=${framework_name}/Modules/AttributeGraph.swiftmodule
+VERSION=RELEASE_2021
 
 generate_swiftinterface_header() {
     local target="$1"
@@ -31,30 +29,37 @@ generate_swiftinterface() {
 }
 
 generate_framework() {
-    local frmework_name=$1
+    local framework_name=$1
+    local arch_name=$2
 
-    rm -rf ${xcframework_path}/${frmework_name}/${framework_name}/Headers
-    rm -rf ${xcframework_path}/${frmework_name}/${framework_name}/Modules
-    rm -rf ${xcframework_path}/${frmework_name}/${framework_name}/Info.plist
+    local path="${AG_ROOT}/${framework_name}.xcframework/${arch_name}/${framework_name}.framework"
 
-    cp -rf ${AG_ROOT}/Sources/Headers ${xcframework_path}/${frmework_name}/${framework_name}/
-    cp -rf ${AG_ROOT}/Sources/Modules ${xcframework_path}/${frmework_name}/${framework_name}/
-    cp -rf ${AG_ROOT}/Sources/Info.plist ${xcframework_path}/${frmework_name}/${framework_name}/
+    rm -rf ${path}/${framework_name}.tbd
+    rm -rf ${path}/Headers
+    rm -rf ${path}/Modules
+    rm -rf ${path}/Info.plist
 
-    cd ${xcframework_path}/${frmework_name}/${swiftmodule_path}/
+    cp ${AG_ROOT}/tbds/${VERSION}/${arch_name}/${framework_name}.tbd ${path}/
+    cp -rf ${AG_ROOT}/Sources/Headers ${path}/
+    cp -rf ${AG_ROOT}/Sources/Modules ${path}/
+    cp -rf ${AG_ROOT}/Sources/Info.plist ${path}/
+
+    cd ${path}/Modules/${framework_name}.swiftmodule
 }
 
-generate_framework ios-arm64-x86_64-simulator
+framework_name=AttributeGraph
+
+generate_framework $framework_name ios-arm64-x86_64-simulator
 generate_swiftinterface x86_64-apple-ios-simulator x86_64-apple-ios11.0-simulator
 generate_swiftinterface arm64-apple-ios-simulator arm64-apple-ios11.0-simulator
 rm template.swiftinterface
 
-generate_framework ios-arm64-arm64e
+generate_framework $framework_name ios-arm64-arm64e
 generate_swiftinterface arm64-apple-ios arm64e-apple-ios11.0 
 generate_swiftinterface arm64e-apple-ios arm64e-apple-ios11.0 
 rm template.swiftinterface
 
-generate_framework macos-arm64e-arm64-x86_64
+generate_framework $framework_name macos-arm64e-arm64-x86_64
 generate_swiftinterface x86_64-apple-macos x86_64-apple-macos12.0
 generate_swiftinterface arm64-apple-macos arm64-apple-macos12.0
 generate_swiftinterface arm64e-apple-macos arm64e-apple-macos12.0
