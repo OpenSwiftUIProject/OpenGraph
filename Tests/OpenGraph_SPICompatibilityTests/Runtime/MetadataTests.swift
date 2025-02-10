5992cb5119cf2a7d294a535d76e6e6e1ef0d0ef4
Kyle <kyle201817146@gmail.com>
2025-02-11 01:26:38 +0800

    Update OGType Enum stuff (#100)

--- Tests/OpenGraph_SPICompatibilityTests/Runtime/MetadataTests.swift
+++ Tests/OpenGraph_SPICompatibilityTests/Runtime/MetadataTests.swift
@@ -3,7 +3,9 @@
 //  OpenGraph_SPICompatibilityTests
 
 import Testing
+import Numerics
 
+@Suite(.enabled(if: swiftToolchainSupported))
 struct MetadataTests {
     class T1 {
         var a = 0
@@ -18,14 +20,21 @@ struct MetadataTests {
     enum T3 {
         case a, b
     }
-    
+
+    enum T4: Equatable {
+        case none
+        case empty
+        case int(Int)
+        case double(Double)
+    }
+
     protocol P {
         var a: Int { get }
         var b: Double { get }
     }
-    
-    @Test(.enabled(if: swiftToolchainSupported))
-    func kind() throws {
+
+    @Test
+    func kind() {
         #expect(Metadata(T1.self).kind == .class)
         #expect(Metadata(T2.self).kind == .struct)
         #expect(Metadata(T3.self).kind == .enum)
@@ -47,9 +56,54 @@ struct MetadataTests {
         #expect(Metadata(P.Protocol.self).kind == .metatype)
         #expect(Metadata(type(of: Int.self)).kind == .metatype)
     }
-    
+
+    @Test
+    func enumType() {
+        var none = T4.none
+        var empty = T4.empty
+        var int = T4.int(1)
+        var double = T4.double(2.0)
+        let metadata = Metadata(T4.self)
+
+        withUnsafePointer(to: none) { #expect(metadata.enumTag($0) == 2) }
+        withUnsafePointer(to: empty) { #expect(metadata.enumTag($0) == 3) }
+        withUnsafePointer(to: int) { #expect(metadata.enumTag($0) == 0) }
+        withUnsafePointer(to: double) { #expect(metadata.enumTag($0) == 1) }
+
+        withUnsafeMutablePointer(to: &int) {
+            metadata.projectEnumData($0)
+            #expect(UnsafeMutableRawPointer($0).assumingMemoryBound(to: Int.self).pointee == 1)
+        }
+        withUnsafeMutablePointer(to: &double) {
+            metadata.projectEnumData($0)
+            #expect(UnsafeMutableRawPointer($0).assumingMemoryBound(to: Double.self).pointee.isApproximatelyEqual(to: 2.0))
+        }
+        withUnsafeMutablePointer(to: &none) {
+            #expect($0.pointee == .none)
+            metadata.injectEnumTag(tag: 3, $0)
+            #expect($0.pointee == .empty)
+        }
+        withUnsafeMutablePointer(to: &empty) {
+            #expect($0.pointee == .empty)
+            metadata.injectEnumTag(tag: 2, $0)
+            #expect($0.pointee == .none)
+        }
+        withUnsafeMutablePointer(to: &int) {
+            #expect($0.pointee == .int(1))
+
+            metadata.injectEnumTag(tag: 1, $0)
+            #expect(metadata.enumTag($0) == 1)
+            #expect($0.pointee == .double(Double(bitPattern: 1)))
+
+            metadata.injectEnumTag(tag: 2, $0)
+            #expect($0.pointee == .none)
+
+            metadata.injectEnumTag(tag: 3, $0)
+            #expect($0.pointee == .empty)
+        }
+    }
+
     #if OPENGRAPH_SUPPORT_2024_API
-    @Test(.enabled(if: swiftToolchainSupported))
     func descriptor() {
         let t1 = Metadata(T1.self).descriptor
         let t2 = Metadata(T2.self).descriptor
@@ -67,8 +121,8 @@ struct MetadataTests {
         #expect(t1 == Metadata(T1.self).descriptor)
     }
     #endif
-    
-    @Test(.enabled(if: swiftToolchainSupported))
+
+    @Test
     func nominalDescriptor() {
         let t1 = Metadata(T1.self).nominalDescriptor
         let t2 = Metadata(T2.self).nominalDescriptor
@@ -82,8 +136,8 @@ struct MetadataTests {
         #expect(p == nil)
         #expect(optionalP != nil)
     }
-    
-    @Test(.enabled(if: swiftToolchainSupported))
+
+    @Test
     func nominalDescriptorName() throws {
         let t1 = Metadata(T1.self).nominalDescriptorName
         let t2 = Metadata(T2.self).nominalDescriptorName
@@ -97,4 +151,9 @@ struct MetadataTests {
         #expect(p == nil)
         try #expect(String(cString: #require(optionalP)) == "Optional")
     }
+}
+
+extension Metadata {
+    @_silgen_name("OGTypeGetEnumTag")
+    static func getEnumTag<V>(_ value: V) -> UInt32
 }
