c3ca4da62a27c9790660f111be7c7abe5b84bd80
Kyle <kyle201817146@gmail.com>
2025-03-17 00:09:44 +0800

    Update ptr.hpp

---
+++ Sources/OpenGraph_SPI/Data/page_const.hpp
@@ -0,0 +1,28 @@
+//
+//  page_const.hpp
+//  OpenGraph_SPI
+
+#ifndef page_const_hpp
+#define page_const_hpp
+
+#include "OGBase.h"
+
+namespace OG {
+namespace data {
+
+constexpr const uint32_t page_mask_bits = 9;
+
+/// 0x200
+constexpr const uint32_t page_size = 1 << page_mask_bits;
+
+/// 0x1FF
+constexpr const uint32_t page_mask = page_size - 1;
+
+/// 0xFFFF_FE00
+constexpr const uintptr_t page_alignment = ~page_mask;
+
+} /* data */
+} /* OG */
+
+
+#endif /* page_const_hpp */
