ef74847de8effff238ae6480156bf861e0eefd47
Kyle <kyle201817146@gmail.com>
2025-03-17 00:17:51 +0800

    Update zone, table and page

--- Sources/OpenGraph_SPI/Data/page.cpp
+++
@@ -1,5 +0,0 @@
-//
-//  page.cpp
-//  OpenGraph_SPI
-
-#include "page.hpp"
--- Sources/OpenGraph_SPI/Data/table.cpp
+++ Sources/OpenGraph_SPI/Data/table.cpp
@@ -7,6 +7,7 @@
 
 #include "table.hpp"
 #include "page.hpp"
+#include "page.hpp"
 #include "../Util/assert.hpp"
 #include <sys/mman.h>
 #include <malloc/malloc.h>
@@ -21,40 +22,34 @@ uint8_t _shared_table_bytes[sizeof(table) / sizeof(uint8_t)] = {};
 
 malloc_zone_t *_Nullable _malloc_zone;
 
-void table::print() const OG_NOEXCEPT {
-    os_unfair_lock_lock(&_lock);
-    fprintf(stderr, "data::table %p:\n  %.2fKB allocated, %.2fKB used, %.2fKB reusable.\n",
-            this,
-            double(_region_capacity - page_size) / 1024.0,
-            double(this->used_pages_num()) / 1024.0,
-            double(_reusable_pages_num) / 1024.0);
-    os_unfair_lock_unlock(&_lock);
+table &table::ensure_shared() {
+    static dispatch_once_t once;
+    dispatch_once_f(&once, nullptr, [](void *_Nullable context){
+         new (_shared_table_bytes) table();
+    });
+    return shared();
 }
 
-//uint64_t table::raw_page_seed(ptr<page> page) {
-//    page.assert_valid();
-//
-//    lock();
-//
-//    uint32_t page_index = (page / page_size) - 1;
-//    uint32_t map_index = page_index / pages_per_map;
-//
-//    uint64_t result = 0;
-//    if (map_index < _page_metadata_maps.size() && _page_metadata_maps[map_index].test(page_index % page_size)) {
-//        auto raw_zone_info = page->zone->info().to_raw_value();
-//        result = raw_zone_info | (1 < 8);
-//    }
-//
-//    unlock();
-//
-//    return result;
-//}
+table &table::shared() { return *reinterpret_cast<data::table *>(&_shared_table_bytes); }
 
-// dealloc_page_locked(OG::data::ptr<OG::data::page>
-
-// alloc_page(OG::data::zone&, unsigned int)
-
-// make_pages_reusable(unsigned int, bool)
+// FIXME
+table::table() {
+    constexpr vm_size_t initial_size = 32 * pages_per_map * page_size;
+    _region_capacity = initial_size;
+    void *region = mmap(nullptr, initial_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
+    _region_base = reinterpret_cast<vm_address_t>(region);
+    OGGraphVMRegionBaseAddress = region;
+    if (region == MAP_FAILED) {
+        OG::precondition_failure("memory allocation failure (%u bytes, %u)", _region_capacity, errno);
+    }
+    _data_base = reinterpret_cast<vm_address_t>(region) - page_size;
+    _data_capacity = initial_size + page_size;
+    if (_malloc_zone == nullptr) {
+        malloc_zone_t *zone = malloc_create_zone(0, 0);
+        _malloc_zone = zone;
+        malloc_set_zone_name(zone, "OpenGraph graph data");
+    }
+}
 
 void table::grow_region() OG_NOEXCEPT {
     uint32_t new_size = 4 * _region_capacity;
@@ -91,34 +86,47 @@ void table::grow_region() OG_NOEXCEPT {
     _data_capacity = new_size + page_size;
 }
 
-// FIXME
-table::table() {
-    constexpr vm_size_t initial_size = 32 * pages_per_map * page_size;
-    _region_capacity = initial_size;
-    void *region = mmap(nullptr, initial_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
-    _region_base = reinterpret_cast<vm_address_t>(region);
-    OGGraphVMRegionBaseAddress = region;
-    if (region == MAP_FAILED) {
-        OG::precondition_failure("memory allocation failure (%u bytes, %u)", _region_capacity, errno);
-    }
-    _data_base = reinterpret_cast<vm_address_t>(region) - page_size;
-    _data_capacity = initial_size + page_size;
-    if (_malloc_zone == nullptr) {
-        malloc_zone_t *zone = malloc_create_zone(0, 0);
-        _malloc_zone = zone;
-        malloc_set_zone_name(zone, "OpenGraph graph data");
-    }
+void table::make_pages_reusable(uint32_t page_index, bool flag) OG_NOEXCEPT {
+    precondition_failure("TODO");
 }
 
-table &table::ensure_shared() {
-    static dispatch_once_t once;
-    dispatch_once_f(&once, nullptr, [](void *_Nullable context){
-         new (_shared_table_bytes) table();
-    });
-    return shared();
+ptr<page> table::alloc_page(zone *zone, uint32_t size) OG_NOEXCEPT {
+    precondition_failure("TODO");
 }
 
-table &table::shared() { return *reinterpret_cast<data::table *>(&_shared_table_bytes); }
+void table::dealloc_page_locked(ptr<page> page) OG_NOEXCEPT {
+    precondition_failure("TODO");
+}
+
+uint64_t table::raw_page_seed(ptr<page> page) OG_NOEXCEPT {
+    precondition_failure("TODO");
+//    page.assert_valid();
+//
+//    lock();
+//
+//    uint32_t page_index = (page / page_size) - 1;
+//    uint32_t map_index = page_index / pages_per_map;
+//
+//    uint64_t result = 0;
+//    if (map_index < _page_metadata_maps.size() && _page_metadata_maps[map_index].test(page_index % page_size)) {
+//        auto raw_zone_info = page->zone->info().to_raw_value();
+//        result = raw_zone_info | (1 < 8);
+//    }
+//
+//    unlock();
+//
+//    return result;
+}
+
+void table::print() const OG_NOEXCEPT {
+    os_unfair_lock_lock(&_lock);
+    fprintf(stderr, "data::table %p:\n  %.2fKB allocated, %.2fKB used, %.2fKB reusable.\n",
+            this,
+            double(_region_capacity - page_size) / 1024.0,
+            double(this->used_pages_num()) / 1024.0,
+            double(_reusable_pages_num) / 1024.0);
+    os_unfair_lock_unlock(&_lock);
+}
 
 } /* data */
 } /* OG */
