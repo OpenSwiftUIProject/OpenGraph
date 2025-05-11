ebf3e53e0fe56e67ca08a659a06bfcfb5935a3d6
Kyle <kyle201817146@gmail.com>
2025-05-11 23:46:10 +0800

    Add -no-verify-emitted-module-interface

---
+++ Scripts/build_swiftinterface.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+# A `realpath` alternative using the default C implementation.
+filepath() {
+  [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+OPENGRAPH_ROOT="$(dirname $(dirname $(filepath $0)))"
+
+cd $OPENGRAPH_ROOT
+
+swift build -Xswiftc -emit-module-interface -Xswiftc -enable-library-evolution -Xswiftc -no-verify-emitted-module-interface
