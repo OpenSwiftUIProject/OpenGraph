d125de116cfd5406c3543b0efb16d160bca07d9a
Kyle <kyle201817146@gmail.com>
2025-05-18 23:04:30 +0800

    Update OGChangedValueFlags

--- Sources/OpenGraph_SPI/Attribute/OGChangedValueFlags.h
+++ Sources/OpenGraph_SPI/Attribute/OGChangedValueFlags.h
@@ -11,8 +11,8 @@
 #include "OGBase.h"
 
 typedef OG_OPTIONS(uint32_t, OGChangedValueFlags) {
-    OGChangedValueFlags_0 = 0,
     OGChangedValueFlags_1 = 1 << 0,
+    OGChangedValueFlagsRequiresMainThread = 1 << 1,
 };
 
 #endif /* OGChangedValueFlags_h */
