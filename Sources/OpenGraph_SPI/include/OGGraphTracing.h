c35c4812e5b0803153bbf09a95e28a115d44e798
Kyle <kyle201817146@gmail.com>
2025-06-21 18:15:21 +0800

    Add OGGraphTracing API (#127)
    * Add OGGraphTracing API
    
    * Remove openGraphSPICompatibilityTestTarget

---
+++ Sources/OpenGraph_SPI/include/OGGraphTracing.h
@@ -0,0 +1,37 @@
+//
+//  OGGraphTracing.h
+//  OpenGraph_SPI
+
+#ifndef OGGraphTracing_hpp
+#define OGGraphTracing_hpp
+
+#include "OGBase.h"
+#include "OGGraph.h"
+
+typedef OG_OPTIONS(uint32_t, OGGraphTraceFlags) {
+    OGGraphTraceFlags_0 = 0,
+    OGGraphTraceFlags_1 = 1 << 0,
+    OGGraphTraceFlags_2 = 1 << 1,
+} OG_SWIFT_NAME(OGGraphRef.TraceFlags);
+
+OG_ASSUME_NONNULL_BEGIN
+
+OG_EXTERN_C_BEGIN
+
+OG_EXPORT
+OG_REFINED_FOR_SWIFT
+void OGGraphStartTracing(_Nullable OGGraphRef graph, OGGraphTraceFlags options) OG_SWIFT_NAME(OGGraphRef.startTracing(_:options:));
+
+OG_EXPORT
+OG_REFINED_FOR_SWIFT
+void OGGraphStartTracing2(_Nullable OGGraphRef graph, OGGraphTraceFlags options, _Nullable CFArrayRef array);
+
+OG_EXPORT
+OG_REFINED_FOR_SWIFT
+void OGGraphStopTracing(_Nullable OGGraphRef graph) OG_SWIFT_NAME(OGGraphRef.stopTracing(_:));
+
+OG_EXTERN_C_END
+
+OG_ASSUME_NONNULL_END
+
+#endif /* OGGraphTracing_hpp */
--- Tests/OpenGraph_SPICompatibilityTests/README.md
+++
@@ -1,7 +0,0 @@
-## OpenGraphCompatibilityTests
-
-Test public API of OpenGraph_SPI and run it against with AttributeGraph on Apple Platform.
-
-The current supported OS 
-- macOS 14 (CI Supported)
-- iOS 15 ~ 17 on Simulator (CI is not supported)
