d00ed65cc27d55ab213e920312ba6b5bc45d4509
Kyle <kyle201817146@gmail.com>
2025-08-02 21:48:39 +0800

    Fix OGVersion duplicate version issue

--- Sources/OpenGraphCxx/Version/OGVersion.c
+++ Sources/OpenGraphCxx/Version/OGVersion.c
@@ -6,11 +6,11 @@
 #include <OpenGraph/OGVersion.h>
 
 #if OPENGRAPH_RELEASE == OPENGRAPH_RELEASE_2024
-const double OpenGraphVersionNumber __attribute__ ((used)) = (double)6.0;
-const unsigned char OpenGraphVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:OpenGraph  PROJECT:OpenGraph-6.0.87\n";
+//const double OpenGraphVersionNumber __attribute__ ((used)) = (double)6.0;
+//const unsigned char OpenGraphVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:OpenGraph  PROJECT:OpenGraph-6.0.87\n";
 const uint64_t OGVersion = 0x2001e;
 #elif OPENGRAPH_RELEASE == OPENGRAPH_RELEASE_2021
-const double OpenGraphVersionNumber __attribute__ ((used)) = (double)3.2;
-const unsigned char OpenGraphVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:OpenGraph  PROJECT:OpenGraph-3.2.1\n";
+//const double OpenGraphVersionNumber __attribute__ ((used)) = (double)3.2;
+//const unsigned char OpenGraphVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:OpenGraph  PROJECT:OpenGraph-3.2.1\n";
 const uint64_t OGVersion = 0x20014;
 #endif
