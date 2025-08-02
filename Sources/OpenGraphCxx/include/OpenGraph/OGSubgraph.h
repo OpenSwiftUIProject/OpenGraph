358eb3da5d1e28515fd75d065c85bb46c07ce645
James Moschou <james.moschou@gmail.com>
2025-08-02 16:52:01 +0200

    Add OGGraphIntersects

--- Sources/OpenGraphCxx/include/OpenGraph/OGSubgraph.h
+++ Sources/OpenGraphCxx/include/OpenGraph/OGSubgraph.h
@@ -70,6 +70,10 @@ void OGSubgraphRemoveChild(OGSubgraphRef parent, OGSubgraphRef child) OG_SWIFT_N
 
 OG_EXPORT
 OG_REFINED_FOR_SWIFT
+bool OGSubgraphIntersects(OGSubgraphRef subgraph, OGAttributeFlags flags) OG_SWIFT_NAME(OGSubgraphRef.intersects(self:flags:));
+
+OG_EXPORT
+OG_REFINED_FOR_SWIFT
 void OGSubgraphApply(OGSubgraphRef cf_subgraph,
                      OGAttributeFlags flags,
                      const void (*function)(const void * _Nullable context OG_SWIFT_CONTEXT, OGAttribute attribute) OG_SWIFT_CC(swift),
