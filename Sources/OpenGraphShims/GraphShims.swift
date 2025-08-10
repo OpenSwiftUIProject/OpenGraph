fc18e8209d563cc22b6c4b60407cfbf6e7ea939c
Kyle <kyle201817146@gmail.com>
2025-08-11 04:22:31 +0800

    Remove typealias

--- Sources/OpenGraphShims/GraphShims.swift
+++ Sources/OpenGraphShims/GraphShims.swift
@@ -5,16 +5,13 @@
 #if OPENGRAPH_ATTRIBUTEGRAPH
 @_exported public import AttributeGraph
 public typealias OGAttributeInfo = AGAttributeInfo
-public typealias OGAttributeType = AGAttributeType
 public typealias OGCachedValueOptions = AGCachedValueOptions
 public typealias OGChangedValueFlags = AGChangedValueFlags
-public typealias OGGraphCounterQueryType = AGCounterQueryType
 public typealias OGInputOptions = AGInputOptions
 public typealias OGTypeApplyOptions = AGTypeApplyOptions
 public typealias OGUniqueID = AGUniqueID
 public typealias OGValue = AGValue
 public typealias OGValueOptions = AGValueOptions
-public typealias OGValueState = AGValueState
 public let attributeGraphEnabled = true
 public let swiftToolchainSupported = true
 #else
