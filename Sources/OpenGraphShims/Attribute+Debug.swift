bd16ff7bb6e2e30c5c993c1db25c64f14a354d99
Kyle <kyle201817146@gmail.com>
2025-08-19 23:45:13 +0800

    Add indent support

--- Sources/OpenGraphShims/Attribute+Debug.swift
+++ Sources/OpenGraphShims/Attribute+Debug.swift
@@ -4,51 +4,82 @@
 
 extension Attribute: Swift.CustomDebugStringConvertible {
     public var debugDescription: String {
-        #"""
-        Attribute<\#(Value.self)> {
-        \#(identifier.debugDescription)
-        }
+        _debugDescription(indent: 0)
+    }
+    
+    func _debugDescription(indent: Int) -> String {
+        let tabs = String(repeating: "\t", count: indent)
+        return #"""
+        \#(tabs)Attribute<\#(Value.self)> {
+        \#(identifier._debugDescription(indent: indent + 1))
+        \#(tabs)}
         """#
     }
 }
 
 extension AnyAttribute: Swift.CustomDebugStringConvertible {
     public var debugDescription: String {
+        _debugDescription(indent: 0)
+    }
+    
+    func _debugDescription(indent: Int) -> String {
+        let tabs = String(repeating: "\t", count: indent)
+        
         guard self != .nil else {
-            return "AnyAttribute.nil"
+            return "\(tabs)AnyAttribute.nil"
         }
+        
         var description = #"""
-        rawValue: \#(rawValue)
-        graph: \#(graph)
+        \#(tabs)rawValue: \#(rawValue)
+        \#(tabs)graph: \#(graph)
         """#
+        
         if rawValue % 2 == 0 { // direct
-            description.append("\n(direct attribute)")
+            description.append("\n\(tabs)(direct attribute)")
 
             let valueType = valueType
-            description.append("\nvalueType: \(valueType)")
+            description.append("\n\(tabs)valueType: \(valueType)")
 
             var value: Any!
             func project1<T>(type: T.Type) {
                 value = unsafeCast(to: type).value
             }
             _openExistential(valueType, do: project1)
-            description.append("\nvalue: \(value!)")
+            description.append("\n\(tabs)value: \(value!)")
 
             let bodyType = _bodyType
-            description.append("\nbodyType: \(bodyType)")
+            description.append("\n\(tabs)bodyType: \(bodyType)")
 
             var bodyValue: Any!
             func project2<T>(type: T.Type) {
                 bodyValue = _bodyPointer.assumingMemoryBound(to: type).pointee
             }
             _openExistential(bodyType, do: project2)
-            description.append("\nbodyValue: \(bodyValue!)")
+            
+            let bodyValueDescription = _formatBodyValue(bodyValue!, indent: indent)
+            description.append("\n\(tabs)bodyValue:\n\(bodyValueDescription)")
 
-        } else { // indrect
-            description.append("\n(indirect attribute)")
-            description.append("\n\nsource attribute:")
-            description.append("\n\(source.debugDescription)")
+        } else { // indirect
+            description.append("\n\(tabs)(indirect attribute)")
+            description.append("\n\(tabs)source attribute:")
+            description.append("\n\(source._debugDescription(indent: indent + 1))")
         }
         return description
+    }
+    
+    private func _formatBodyValue(_ bodyValue: Any, indent: Int) -> String {
+        let bodyValueString = String(describing: bodyValue)
+        let nextTabs = String(repeating: "\t", count: indent + 1)
+        
+        // Check if the body value contains attributes
+        if bodyValueString.contains("Attribute<") {
+            // Split lines and add proper indentation
+            let lines = bodyValueString.components(separatedBy: .newlines)
+            return lines.enumerated().map { index, line in
+                return "\(nextTabs)\(line)"
+            }.joined(separator: "\n")
+        }
+        
+        return "\(nextTabs)\(bodyValueString)"
     }
 }
